efd6b9ea1c435609a80aed8c47240d914e0965b0
P <130880647+FelixYuan-YF@users.noreply.github.com>
2025-03-25 14:07:59 +0800

    Update checkpoints_download.sh

--- tools/checkpoints_download.sh
+++ tools/checkpoints_download.sh
@@ -2,8 +2,7 @@ mkdir -p ./checkpoints/
 cd ./checkpoints/
 export HF_ENDPOINT="https://hf-mirror.com"
 # depth anything
-huggingface-cli download --resume-download LiheYoung/Depth-Anything --local-dir . --include checkpoints/depth_anything_vitl14.pth
-
+wget https://huggingface.co/depth-anything/Depth-Anything-V2-Large/resolve/main/depth_anything_v2_vitl.pth -O Depth-Anything/depth_anything_v2_vitl.pth
 # raft 
 # wget with proxy or download from google drive: https://drive.google.com/drive/folders/1sWDsfuZ3Up38EUQt7-JDTT1HcGHuJgvT
 wget https://www.dropbox.com/s/4j4z58wuv8o0mfz/raft_tings.pth -O raft/raft_tings.pth # -e "http_proxy=http://127.0.0.1:8087" 
