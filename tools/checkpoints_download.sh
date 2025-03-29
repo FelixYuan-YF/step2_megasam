42b5d39383067ba22959399662a966ede5688319
FelixYuan-YF <FelixYuan2003@outlook.com>
2025-03-29 12:55:59 +0800

    update

--- tools/checkpoints_download.sh
+++ tools/checkpoints_download.sh
@@ -1,8 +1,12 @@
 mkdir -p ./checkpoints/
 cd ./checkpoints/
+mkdir -p Depth-Anything
+mkdir -p raft
+
 export HF_ENDPOINT="https://hf-mirror.com"
 # depth anything
 wget https://huggingface.co/depth-anything/Depth-Anything-V2-Large/resolve/main/depth_anything_v2_vitl.pth -O Depth-Anything/depth_anything_v2_vitl.pth
+
 # raft 
 # wget with proxy or download from google drive: https://drive.google.com/drive/folders/1sWDsfuZ3Up38EUQt7-JDTT1HcGHuJgvT
 wget https://www.dropbox.com/s/4j4z58wuv8o0mfz/raft_tings.pth -O raft/raft_tings.pth # -e "http_proxy=http://127.0.0.1:8087" 
